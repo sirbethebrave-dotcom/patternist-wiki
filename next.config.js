4cb55bcd9c6cb255ff5076811d9294af9e50c647
Юлия Ларионова <yualarionova@sc-mac-00970.corp.sbercloud.ru>
2025-08-26 01:29:45 +0300

    Исправлена конфигурация Next.js и добавлены инструкции по развертыванию

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // App Router включен по умолчанию в Next.js 14
 }
 
 module.exports = nextConfig
